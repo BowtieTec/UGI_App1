a167c57b31d28d7e99ea4f893a5b6794d532d81a
acordovam <alejandrocordova198@gmail.com>
2021-10-11 17:14:06 -0600

    Armado y creado de los componentes pertenecientes al home. Creado componentes y armado del dashboard (Hace falta graficas). Rutas ordenadas y segundo router-outlet funcionando, separando el home y el login. Agregando imagenes de ejemplo.

---
+++ src/app/shared/card-kpi/card-kpi.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CardKpiComponent } from './card-kpi.component';
+
+describe('CardKpiComponent', () => {
+  let component: CardKpiComponent;
+  let fixture: ComponentFixture<CardKpiComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CardKpiComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CardKpiComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LeftPanelComponent } from './left-panel/left-panel.component';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { LocationPanelComponent } from './location-panel/location-panel.component';
+import { CardKpiComponent } from './card-kpi/card-kpi.component';
+import { BarChartComponent } from './bar-chart/bar-chart.component';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
+  declarations: [
+    LeftPanelComponent,
+    HeaderComponent,
+    LocationPanelComponent,
+    CardKpiComponent,
+    BarChartComponent,
+  ],
+  imports: [CommonModule, RouterModule],
+  exports: [
+    LeftPanelComponent,
+    HeaderComponent,
+    LocationPanelComponent,
+    CardKpiComponent,
+    BarChartComponent,
+  ],
 })
 export class SharedModule {}
