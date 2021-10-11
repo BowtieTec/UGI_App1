7126f1ef6ef4963b0413ccc2dafe3d8a3de42f90
acordovam <alejandrocordova198@gmail.com>
2021-10-11 17:14:06 -0600

    Added Modules and components of the dashboard, home and routes: assembled and created of the components belonging to the home. Created and assembled the dashboard (Graphics required). Ordered routes and second router-outlet working. separating the home and the login by routes. Adding example images.

--- src/app/features/features-routing.module.ts
+++ src/app/features/features-routing.module.ts
@@ -6,6 +6,10 @@ const routes: Routes = [
     path: '',
     loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
   },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+  },
 ];
 
 @NgModule({
---
+++ src/app/shared/card-kpi/card-kpi.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-kpi',
+  templateUrl: './card-kpi.component.html',
+  styleUrls: ['./card-kpi.component.css']
+})
+export class CardKpiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
