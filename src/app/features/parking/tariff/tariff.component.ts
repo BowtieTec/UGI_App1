d3ec7e428b8e2a51f33aea9e79465eb98260dd4a
acordovam <alejandrocordova198@gmail.com>
2021-11-24 16:18:33 -0600

    Added validations at forms in Tariff

--- src/app/features/parking/tariff/tariff.component.ts
+++ src/app/features/parking/tariff/tariff.component.ts
@@ -1,17 +1,55 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UtilitiesService } from '../../../shared/services/utilities.service';
 
 @Component({
   selector: 'app-tariff',
   templateUrl: './tariff.component.html',
   styleUrls: ['./tariff.component.css'],
 })
-export class TariffComponent implements OnInit {
+export class TariffComponent {
   timeRange: number = 1;
   costType: number = 1;
   disableRanges: boolean = false;
+  holidayForm: FormGroup;
+  rankForm: FormGroup;
+  blockForm: FormGroup;
 
-  ngOnInit(): void {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private utilitiesService: UtilitiesService
+  ) {
+    this.holidayForm = this.createHolidayOrRankForm();
+    this.rankForm = this.createHolidayOrRankForm();
+    this.blockForm = this.createBlockForm();
+  }
 
-  saveRule() {}
+  saveRule() {
+    return;
+  }
+
+  validateHolidayForm(control: string) {
+    return this.utilitiesService.controlInvalid(this.holidayForm, control);
+  }
+
+  validateRankForm(control: string) {
+    return this.utilitiesService.controlInvalid(this.rankForm, control);
+  }
+
+  validateBlockForm(control: string) {
+    return this.utilitiesService.controlInvalid(this.blockForm, control);
+  }
+
+  createHolidayOrRankForm() {
+    return this.formBuilder.group({
+      from: ['', Validators.required],
+      to: ['', Validators.required],
+    });
+  }
+
+  createBlockForm() {
+    return this.formBuilder.group({
+      blockSize: ['', Validators.required],
+    });
+  }
 }
