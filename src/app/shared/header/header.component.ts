e9755e0611e62bddca4bf0612db0e9b14c27e252
acordovam <alejandrocordova198@gmail.com>
2021-11-25 11:11:07 -0600

    Change tag <i> by em for better semantics

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+import { MessageService } from '../services/message.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+  constructor(
+    private auth: AuthService,
+    private rooute: Router,
+    private messageService: MessageService
+  ) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  logout() {
+    this.auth.cleanUser();
+    this.messageService.showLoading();
+    this.rooute.navigate(['/']).then(() => {
+      this.messageService.OkTimeOut('Sesión cerrada');
+    });
   }
-
 }
