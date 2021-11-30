81282c75dc4ae57a78a9a8a500034c6d704ada55
acordovam <alejandrocordova198@gmail.com>
2021-11-30 16:53:29 -0600

    Added menu from parking and management. Added unit permissions to courtesy, users, parking, etc. Added codes for permissions at environments. Apply the permissions for role. Fixed some issues for all project and added futures at tariff in step four.

---
+++ src/app/features/parking/components/parking/parking.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parking',
+  templateUrl: './parking.component.html',
+  styleUrls: ['./parking.component.css'],
+})
+export class ParkingComponent {}
