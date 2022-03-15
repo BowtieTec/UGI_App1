7c2985c174ec9e5adf9a6f1ff684717b719fe10a
aleja <alejandrocordova198@gmail.com>
2022-03-15 16:14:54 -0600

    Added new Guest Token

--- src/app/core/interceptos/auth-interceptor.service.ts
+++ src/app/core/interceptos/auth-interceptor.service.ts
@@ -9,23 +9,33 @@ import {
 import { Observable, throwError } from 'rxjs'
 import { AuthService } from '../../shared/services/auth.service'
 import { catchError } from 'rxjs/operators'
+import { Router } from '@angular/router'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthInterceptorService implements HttpInterceptor {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private routes: Router) {}
 
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
     const token: string = this.authService.getUser().token
+    const tempToken =
+      this.routes.routerState.snapshot.url.split('/registro?t=')[1]
     let request = req
     if (token) {
       request = req.clone({
         setHeaders: {
           Authorization: `Bearer ${token}`
+        }
+      })
+    } else if (tempToken) {
+      console.log(tempToken)
+      request = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${tempToken}`
         }
       })
     }
--- src/app/core/interceptos/error-interceptor.service.ts
+++ src/app/core/interceptos/error-interceptor.service.ts
@@ -1,8 +1,9 @@
 import { ErrorHandler, Injectable } from '@angular/core'
 import { MessageService } from '../../shared/services/message.service'
 import { Router } from '@angular/router'
-import { HttpErrorResponse } from '@angular/common/http'
 import { AuthService } from '../../shared/services/auth.service'
+import { throwError } from 'rxjs'
+import { environment } from '../../../environments/environment'
 
 @Injectable()
 export class GlobalErrorHandler implements ErrorHandler {
@@ -12,48 +13,31 @@ export class GlobalErrorHandler implements ErrorHandler {
     private auth: AuthService
   ) {}
 
-  handleError(error: any) {
+  handleError(error: Response | any) {
     console.log(error)
-    if (!(error instanceof HttpErrorResponse)) {
-      error = error.rejection // get the error object
+    switch (error.status) {
+      case 401:
+        this.message.error('Token vencido. Por favor iniciar sesion nuevamente')
+        this.auth.cleanUser()
+        this.router.navigate(['/'])
+        return
     }
-    if (error.status === 400) {
-      this.message.error(
-        'Datos incorrectos',
-        'Datos faltantes o incorrectos: ' +
-          (error.error.messages != undefined ? error.error.messages : '')
-      )
-      return
+    let errMsg: string
+    if (error instanceof Response) {
+      const body: any = error.json() || ''
+      const err = body.error || JSON.stringify(body)
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`
+      return throwError(errMsg)
+    } else {
+      errMsg = error.message ? error.message : error.toString()
     }
-    if (error.status === 401) {
-      this.message.error(
-        'Sesión vencida',
-        'El tiempo de sesión ha vencido. Por favor vuelva a iniciar sesión.'
-      )
-      this.auth.cleanUser()
-      this.router.navigate(['/'])
-    }
-    if (error.status === 404) {
-      this.message.error(
-        'Servicio no encontrado.',
-        'Por favor verifique que tenga conexión a internet o intente mas tarde.'
-      )
-      return
-    }
-    if (error.status === 409) {
-      if (error.error.message.includes('QueryFailedError')) {
-        this.message.error(
-          '',
-          'Parece haber un error en la base de datos. Intenta seleccionando otros datos. Si necesitas seleccionar estos datos en concreto comunicáselo a tu administrador'
-        )
-      } else {
-        this.message.error('', `${error.error.message}`)
-      }
-      return
-    }
-    if (error.status == 500 || error.status == 501) {
-      this.message.error('', `${error.error.message}`)
-    }
-    this.message.error('Error desconocido', error.error.message)
+    if (!environment.production) console.error('Error: ', errMsg)
+
+    return throwError(errMsg)
+  }
+
+  protected extractData(res: Response) {
+    const body: any = res.json()
+    return body.data || {}
   }
 }
