07156222dafe8f84e852c794d1b118ccd310b20c
Berny Cardona <bcardona@sion.com.gt>
2022-03-22 17:46:28 -0600

    adding courtesy option

--- src/app/features/management/components/tariff-test/tariff-test.component.ts
+++ src/app/features/management/components/tariff-test/tariff-test.component.ts
@@ -9,6 +9,8 @@ import { TariffTestService } from './services/tariff-test.service'
 import { tariffTestModel } from './models/tariff-test.model'
 import { TicketTestModule } from './models/ticket-test.module'
 import { UtilitiesService } from 'src/app/shared/services/utilities.service'
+import { CourtesyService } from '../../../courtesy/services/courtesy.service'
+import { CourtesyModel } from '../../../courtesy/models/Courtesy.model'
 
 @Component({
   selector: 'app-tariff-test',
@@ -18,7 +20,9 @@ import { UtilitiesService } from 'src/app/shared/services/utilities.service'
 export class TariffTestComponent {
   tariffTestForm: FormGroup
   allParkingLot: ParkingModel[] = []
+  courtesies: CourtesyModel[] = []
   ticket: TicketTestModule
+  private courtesyId: string = ''
   private parkingId: string = this.authService.getParking().id
 
   constructor(
@@ -28,6 +32,7 @@ export class TariffTestComponent {
     private parkingService: ParkingService,
     private messageService: MessageService,
     private testService: TariffTestService,
+    private courtesyService: CourtesyService,
     private utilitiesService: UtilitiesService
   ) {
     this.ticket = new TicketTestModule()
@@ -39,11 +44,12 @@ export class TariffTestComponent {
     return this.authService.isSudo
   }
 
-  get formTarifftestValues(): tariffTestModel {
+  get formTariffTestValues(): tariffTestModel {
     return {
       parkingId: this.parkingId,
       entry_date: new Date(this.tariffTestForm.get('date_in')?.value),
-      exit_date: new Date(this.tariffTestForm.get('date_out')?.value)
+      exit_date: new Date(this.tariffTestForm.get('date_out')?.value),
+      courtesyId: this.tariffTestForm.get('courtesyId')?.value
     }
   }
 
@@ -51,18 +57,29 @@ export class TariffTestComponent {
     return this.permissionService.ifHaveAction(action)
   }
 
+  get parkingSelected() {
+    this.parkingId = this.tariffTestForm.get('parking')?.value
+    this.courtesyId = this.tariffTestForm.get('courtesyId')?.value
+    return this.tariffTestForm.get('parking')?.value
+  }
+
+  courtesySelected() {
+    this.courtesyId = this.tariffTestForm.get('courtesyId')?.value
+  }
+
   async getTariffTest() {
     if (this.tariffTestForm.invalid) {
       this.messageService.error('', 'Datos no válidos o faltantes')
       return
     }
-    const newTest = this.formTarifftestValues
-    const newTicket = await this.testService.getTariffTest(newTest)
+    const newTest = this.formTariffTestValues
+    console.log(newTest)
+    /*const newTicket = await this.testService.getTariffTest(newTest)
     this.ticket.amount = newTicket.ticket.amount
     this.ticket.days = newTicket.ticket.days
     this.ticket.hour = newTicket.ticket.hour
     this.ticket.minute = newTicket.ticket.minute
-    this.ticket.tariff = newTicket.ticket.tariff
+    this.ticket.tariff = newTicket.ticket.tariff*/
   }
 
   validateDateForm(control: string) {
@@ -73,11 +90,13 @@ export class TariffTestComponent {
     return this.formBuilder.group({
       parking: [this.parkingId, [Validators.required]],
       date_in: ['', [Validators.required]],
-      date_out: ['', [Validators.required]]
+      date_out: ['', [Validators.required]],
+      courtesyId: [this.courtesyId]
     })
   }
 
   private async getInitialData() {
+    await this.getCourtesies()
     await this.getAllParkingLot()
   }
 
@@ -85,7 +104,23 @@ export class TariffTestComponent {
     return this.parkingService.getAllParking().then((x) => {
       if (x.success) {
         this.allParkingLot = x.data.parkings
+        console.log(this.allParkingLot)
       }
     })
+  }
+
+  getCourtesies(parkingId = this.parkingId) {
+    console.log(this.parkingId)
+    return this.courtesyService
+      .getCourtesys(parkingId)
+      .toPromise()
+      .then((data) => {
+        if (data.success) {
+          this.courtesies = data.data
+          console.log(this.courtesies)
+        } else {
+          this.messageService.error('', data.message)
+        }
+      })
   }
 }
