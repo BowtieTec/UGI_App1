ce28dfa22d9e9e571c1a12c0d46861f55920df9c
acordovam <alejandrocordova198@gmail.com>
2022-04-01 12:32:57 -0600

    Ticket component

--- src/app/features/parking/services/parking.service.ts
+++ src/app/features/parking/services/parking.service.ts
@@ -376,7 +376,6 @@ export class ParkingService {
       .toPromise()
       .then((data) => {
         if (data.success) {
-          console.log(data.data.stations)
           return data.data.stations
         }
         return new Array<StationsCourtesyModel>()
--- src/app/features/support/components/new-ticket/new-ticket.component.ts
+++ src/app/features/support/components/new-ticket/new-ticket.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { ResponseModel } from 'src/app/shared/model/Request.model';
-import { AuthModel } from 'src/app/shared/model/UserResponse.model';
-import { AuthService } from 'src/app/shared/services/auth.service';
-import { MessageService } from 'src/app/shared/services/message.service';
-import { PermissionsService } from 'src/app/shared/services/permissions.service';
-import { UtilitiesService } from 'src/app/shared/services/utilities.service';
-import { supportTicketModel } from './Models/support-ticket.module';
-import { SupportTicketService } from './Services/support-ticket.service';
-
+import { Component, OnInit } from '@angular/core'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { Router } from '@angular/router'
+import { ResponseModel } from 'src/app/shared/model/Request.model'
+import { AuthModel } from 'src/app/shared/model/UserResponse.model'
+import { AuthService } from 'src/app/shared/services/auth.service'
+import { MessageService } from 'src/app/shared/services/message.service'
+import { PermissionsService } from 'src/app/shared/services/permissions.service'
+import { UtilitiesService } from 'src/app/shared/services/utilities.service'
+import { supportTicketModel } from './Models/support-ticket.module'
+import { SupportTicketService } from './Services/support-ticket.service'
 
 @Component({
   selector: 'app-new-ticket',
@@ -17,12 +16,10 @@ import { SupportTicketService } from './Services/support-ticket.service';
   styleUrls: ['./new-ticket.component.css']
 })
 export class NewTicketComponent {
-  
   supportTicketForm: FormGroup
   date: Date = new Date()
   authData: AuthModel = this.authService.getUser()
-  
-  
+
   constructor(
     private permissionService: PermissionsService,
     private formBuilder: FormBuilder,
@@ -31,50 +28,49 @@ export class NewTicketComponent {
     private utilitiesService: UtilitiesService,
     private supportTicketService: SupportTicketService,
     private router: Router
-    ) {
-      this.supportTicketForm = this.createsupportTicketForm()
-    }
-    
-    private createsupportTicketForm() {
-      return this.formBuilder.group({
-        email: ['', [Validators.required]],
-        description: ['', [Validators.required]],
-        subject: ['', [Validators.required]]
-      })
-    }
-    get formSupportTicketValues(): supportTicketModel {
-      return {
-        email: this.authData.user.email,//this.supportTicketForm.get('email')?.value,
-        subject: this.supportTicketForm.get('subject')?.value,
-        description: this.supportTicketForm.get('description')?.value
-      }
-    }
-    
-    async sendSupportTicket() {
-      if (this.supportTicketForm.invalid) {
-        this.messageService.error('', 'Datos no válidos o faltantes')
-        return
-      }
-      const newTicket = this.formSupportTicketValues
-      
-      const response = await this.supportTicketService
-      .sendSupportTicket(newTicket).toPromise()
-      
-      if(response){
-        this.messageService.info("Se ha enviado su petición, pronto recibirá una notificación en su correo")
-        this.reloadComponent()
-      }else{
-        this.messageService.error('', 'No se pudo enviar la solicitud')
-      }
-    }
-
-    reloadComponent() {
-      let currentUrl = this.router.url;
-          this.router.routeReuseStrategy.shouldReuseRoute = () => false;
-          this.router.onSameUrlNavigation = 'reload';
-          this.router.navigate([currentUrl]);
-      }
-    
-
+  ) {
+    this.supportTicketForm = this.createsupportTicketForm()
   }
-  
+
+  private createsupportTicketForm() {
+    return this.formBuilder.group({
+      description: ['', [Validators.required]],
+      subject: ['', [Validators.required]]
+    })
+  }
+  get formSupportTicketValues(): supportTicketModel {
+    return {
+      email: this.authData.user.email, //this.supportTicketForm.get('email')?.value,
+      subject: this.supportTicketForm.get('subject')?.value,
+      description: this.supportTicketForm.get('description')?.value
+    }
+  }
+
+  async sendSupportTicket() {
+    if (this.supportTicketForm.invalid) {
+      this.messageService.error('', 'Datos no válidos o faltantes')
+      return
+    }
+    const newTicket = this.formSupportTicketValues
+
+    const response = await this.supportTicketService
+      .sendSupportTicket(newTicket)
+      .toPromise()
+
+    if (response) {
+      this.messageService.info(
+        'Se ha enviado su petición, pronto recibirá una notificación en su correo'
+      )
+      this.reloadComponent()
+    } else {
+      this.messageService.error('', 'No se pudo enviar la solicitud')
+    }
+  }
+
+  reloadComponent() {
+    let currentUrl = this.router.url
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false
+    this.router.onSameUrlNavigation = 'reload'
+    this.router.navigate([currentUrl])
+  }
+}
