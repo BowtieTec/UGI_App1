52353f36cec1b3dc8be4e5823320fa7485d06018
aleja <alejandrocordova198@gmail.com>
2022-04-08 10:18:48 -0600

    Added package of toast and loading to create massive courtesies.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,24 @@
-import { ErrorHandler, LOCALE_ID, NgModule } from '@angular/core'
-import { BrowserModule } from '@angular/platform-browser'
-import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component'
-import { FormsModule, ReactiveFormsModule } from '@angular/forms'
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http'
-import { GoogleMapsModule } from '@angular/google-maps'
-import { GlobalErrorHandler } from './core/interceptos/error-interceptor.service'
-import { AuthInterceptorService } from './core/interceptos/auth-interceptor.service'
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
-import { DataTablesModule } from 'angular-datatables'
-import { SharedModule } from './shared/shared.module'
-import { CurrencyPipe } from '@angular/common'
-import { RegisterPublicFormComponent } from './register-public-form/register-public-form.component'
-import { ParkingModule } from './features/parking/parking.module'
-import { FileUploadModule } from './register-public-form/components/file-upload.module'
+import {ErrorHandler, LOCALE_ID, NgModule} from '@angular/core'
+import {BrowserModule} from '@angular/platform-browser'
+import {AppRoutingModule} from './app-routing.module'
+import {AppComponent} from './app.component'
+import {FormsModule, ReactiveFormsModule} from '@angular/forms'
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http'
+import {GoogleMapsModule} from '@angular/google-maps'
+import {GlobalErrorHandler} from './core/interceptos/error-interceptor.service'
+import {AuthInterceptorService} from './core/interceptos/auth-interceptor.service'
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap'
+import {DataTablesModule} from 'angular-datatables'
+import {SharedModule} from './shared/shared.module'
+import {CurrencyPipe, registerLocaleData} from '@angular/common'
+import {RegisterPublicFormComponent} from './register-public-form/register-public-form.component'
+import {ParkingModule} from './features/parking/parking.module'
+import {FileUploadModule} from './register-public-form/components/file-upload.module'
 
 import localGT from '@angular/common/locales/es-GT'
-import { registerLocaleData } from '@angular/common'
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {ToastrModule} from "ngx-toastr";
+
 registerLocaleData(localGT, 'es-GT')
 
 @NgModule({
@@ -32,19 +34,29 @@ registerLocaleData(localGT, 'es-GT')
     DataTablesModule,
     SharedModule,
     ParkingModule,
-    FileUploadModule
+    FileUploadModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      timeOut: 30000,
+      positionClass: 'toast-bottom-left',
+      preventDuplicates: true,
+      closeButton: true,
+      progressBar: true,
+
+    })
   ],
   providers: [
-    { provide: ErrorHandler, useClass: GlobalErrorHandler },
-    { provide: LOCALE_ID, useValue: 'es-GT' },
+    {provide: ErrorHandler, useClass: GlobalErrorHandler},
+    {provide: LOCALE_ID, useValue: 'es-GT'},
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptorService,
       multi: true
     },
-    { provide: CurrencyPipe }
+    {provide: CurrencyPipe}
   ],
   bootstrap: [AppComponent],
   exports: []
 })
-export class AppModule {}
+export class AppModule {
+}
