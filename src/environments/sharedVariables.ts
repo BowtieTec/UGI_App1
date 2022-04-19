206cc285b4081e6d7186b949759b376e10746652
acordovam <alejandrocordova198@gmail.com>
2022-04-19 08:41:58 -0600

    Fixed the userContext

--- src/environments/sharedVariables.ts
+++ src/environments/sharedVariables.ts
@@ -114,6 +114,6 @@ export const TypeOfCondition = [
   { id: 2, name: 'Solo si Cant. Horas es menor o igual a ' }
 ]
 export const settings = {
-  siteKey: '6Ld6eIIfAAAAAOMY2BQdV4tw9ZAxIy7l2zwVETnT'
+  siteKey: '6LesloIfAAAAAGnZMEasksxaC9oD6NcDkIVzRePH'
 }
 
