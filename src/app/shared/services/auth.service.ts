47237f07fc99102aecbc9b507c4edba0c8191620
aleja <alejandrocordova198@gmail.com>
2022-04-22 10:49:20 -0600

    Fixed recaptcha

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,13 +1,14 @@
-import { Injectable } from '@angular/core'
-import { UserRequestModel } from '../model/UserRequest.model'
-import { HttpClient } from '@angular/common/http'
-import { AuthModel, ParkingAuthModel, UserResponseModel } from '../model/UserResponse.model'
-import { environment } from '../../../environments/environment'
-import { MessageService } from './message.service'
-import { EncryptionService } from './encryption.service'
-import { Router } from '@angular/router'
-import { sha512 } from 'js-sha512'
-import { UtilitiesService } from './utilities.service'
+import {Injectable} from '@angular/core'
+import {UserRequestModel} from '../model/UserRequest.model'
+import {HttpClient} from '@angular/common/http'
+import {AuthModel, ParkingAuthModel, UserResponseModel} from '../model/UserResponse.model'
+import {environment} from '../../../environments/environment'
+import {MessageService} from './message.service'
+import {EncryptionService} from './encryption.service'
+import {Router} from '@angular/router'
+import {sha512} from 'js-sha512'
+import {UtilitiesService} from './utilities.service'
+import {ReCaptchaV3Service} from "ng-recaptcha";
 
 
 @Injectable({
@@ -22,7 +23,8 @@ export class AuthService {
     private message: MessageService,
     private crypto: EncryptionService,
     private route: Router,
-    private utilities: UtilitiesService
+    private utilities: UtilitiesService,
+    private recaptcha: ReCaptchaV3Service
   ) {
     this.userContext = sha512(this.utilities.randomString())
   }
@@ -55,19 +57,19 @@ export class AuthService {
   }
 
   login(login: UserRequestModel) {
-    /*this.recaptcha.execute('login')
-      .subscribe((token: string) => {*/
-    //login.userContext = token
-    this.message.showLoading()
-    this.http
-      .post<UserResponseModel>(`${this.apiUrl}backoffice/admin/signin`, login)
-      .toPromise()
-      .then((data) => {
-        if (data.success) {
-          this.saveUser(data.data)
-          this.message.OkTimeOut('!Listo!')
-          this.route.navigate(['/home']).catch()
-        } else {
+    this.recaptcha.execute('login')
+      .subscribe((token: string) => {
+        login.userContext = token
+        this.message.showLoading()
+        this.http
+          .post<UserResponseModel>(`${this.apiUrl}backoffice/admin/signin`, login)
+          .toPromise()
+          .then((data) => {
+            if (data.success) {
+              this.saveUser(data.data)
+              this.message.OkTimeOut('!Listo!')
+              this.route.navigate(['/home']).catch()
+            } else {
               this.cleanUser()
               this.message.error('', data.message)
               this.route.navigate(['/']).catch()
@@ -78,8 +80,8 @@ export class AuthService {
             this.message.error('', data.error.message)
             this.route.navigate(['/']).catch()
           })
-    /* }, (err) => {
-       throw new Error('Error: No pudo completarse el reCAPTCHA. Vuelva a iniciar sesión.')
-     })*/
+      }, (err) => {
+        throw new Error('Error: No pudo completarse el reCAPTCHA. Vuelva a iniciar sesión.')
+      })
   }
 }
