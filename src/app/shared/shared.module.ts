518f690aebd1c3df9797bb94cdbb2d014d876ad7
aleja <alejandrocordova198@gmail.com>
2022-07-27 13:52:03 -0600

    New Placeholder of targets

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,24 +1,25 @@
-import { NgModule } from '@angular/core'
-import { CommonModule } from '@angular/common'
-import { LeftPanelComponent } from './left-panel/left-panel.component'
-import { RouterModule } from '@angular/router'
-import { HeaderComponent } from './header/header.component'
-import { LocationPanelComponent } from './location-panel/location-panel.component'
-import { CardKpiComponent } from './card-kpi/card-kpi.component'
-import { BarChartComponent } from './bar-chart/bar-chart.component'
-import { LoadingComponent } from './loading/loading.component'
-import { TimePipe } from './pipes/time.pipe'
-import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap'
-import { FilterPipe } from './pipes/filter.pipe'
-import { Page404Component } from './page404/page404.component'
-import { InputContainerComponent } from './forms/input-container/input-container.component'
-import { ReactiveFormsModule } from '@angular/forms'
-import { SelectContainerComponent } from './forms/select-container/select-container.component'
-import { TextAreaContainerComponent } from './forms/text-area-container/text-area-container.component'
-import { CheckboxContainerComponent } from './forms/checkbox-container/checkbox-container.component'
-import { CourtesyValuePipe } from './pipes/courtesy-value.pipe'
-import { CourtesyChartComponent } from './courtesy-chart/courtesy-chart.component'
-import { RadioContainerComponent } from './forms/radio-container/radio-container.component'
+import {NgModule} from '@angular/core'
+import {CommonModule} from '@angular/common'
+import {LeftPanelComponent} from './left-panel/left-panel.component'
+import {RouterModule} from '@angular/router'
+import {HeaderComponent} from './header/header.component'
+import {LocationPanelComponent} from './location-panel/location-panel.component'
+import {CardKpiComponent} from './card-kpi/card-kpi.component'
+import {BarChartComponent} from './bar-chart/bar-chart.component'
+import {LoadingComponent} from './loading/loading.component'
+import {TimePipe} from './pipes/time.pipe'
+import {NgbPaginationModule} from '@ng-bootstrap/ng-bootstrap'
+import {FilterPipe} from './pipes/filter.pipe'
+import {Page404Component} from './page404/page404.component'
+import {InputContainerComponent} from './forms/input-container/input-container.component'
+import {ReactiveFormsModule} from '@angular/forms'
+import {SelectContainerComponent} from './forms/select-container/select-container.component'
+import {TextAreaContainerComponent} from './forms/text-area-container/text-area-container.component'
+import {CheckboxContainerComponent} from './forms/checkbox-container/checkbox-container.component'
+import {CourtesyValuePipe} from './pipes/courtesy-value.pipe'
+import {CourtesyChartComponent} from './courtesy-chart/courtesy-chart.component'
+import {RadioContainerComponent} from './forms/radio-container/radio-container.component';
+import {CardPlaceholderComponent} from './card-placeholder/card-placeholder.component'
 
 @NgModule({
   declarations: [
@@ -37,7 +38,8 @@ import { RadioContainerComponent } from './forms/radio-container/radio-container
     CheckboxContainerComponent,
     CourtesyValuePipe,
     CourtesyChartComponent,
-    RadioContainerComponent
+    RadioContainerComponent,
+    CardPlaceholderComponent
   ],
   imports: [
     CommonModule,
@@ -63,4 +65,5 @@ import { RadioContainerComponent } from './forms/radio-container/radio-container
     RadioContainerComponent
   ]
 })
-export class SharedModule {}
+export class SharedModule {
+}
