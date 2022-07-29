cf80be0876154add63256b84c8cf353174f22761
aleja <alejandrocordova198@gmail.com>
2022-07-29 11:19:09 -0600

    (E-205) Adding Observer of Parking and parking lot to Create  component and Adding Reactive Forms to the courtesy report.

--- src/app/features/report/components/courtesy-report/courtesy-report.component.ts
+++ src/app/features/report/components/courtesy-report/courtesy-report.component.ts
@@ -1,22 +1,23 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core'
-import { DataTableDirective } from 'angular-datatables'
-import { Subject } from 'rxjs'
-import { MessageService } from '../../../../shared/services/message.service'
-import { DataTableOptions } from '../../../../shared/model/DataTableOptions'
-import { ReportService } from '../service/report.service'
-import { UtilitiesService } from '../../../../shared/services/utilities.service'
-import { AuthService } from '../../../../shared/services/auth.service'
-import { PermissionsService } from '../../../../shared/services/permissions.service'
-import { environment } from 'src/environments/environment'
-import { jsPDF } from 'jspdf'
-import { DxDataGridComponent } from 'devextreme-angular'
-import { exportDataGrid as exportDataGridToPdf } from 'devextreme/pdf_exporter'
-import { Workbook } from 'exceljs'
-import { saveAs } from 'file-saver'
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core'
+import {DataTableDirective} from 'angular-datatables'
+import {Subject} from 'rxjs'
+import {MessageService} from '../../../../shared/services/message.service'
+import {DataTableOptions} from '../../../../shared/model/DataTableOptions'
+import {ReportService} from '../service/report.service'
+import {UtilitiesService} from '../../../../shared/services/utilities.service'
+import {AuthService} from '../../../../shared/services/auth.service'
+import {PermissionsService} from '../../../../shared/services/permissions.service'
+import {environment} from 'src/environments/environment'
+import {jsPDF} from 'jspdf'
+import {DxDataGridComponent} from 'devextreme-angular'
+import {exportDataGrid as exportDataGridToPdf} from 'devextreme/pdf_exporter'
+import {Workbook} from 'exceljs'
+import {saveAs} from 'file-saver'
 import * as logoFile from '../logoEbi'
 
-import { ParkingService } from '../../../parking/services/parking.service'
-import { ParkingModel } from '../../../parking/models/Parking.model'
+import {ParkingService} from '../../../parking/services/parking.service'
+import {ParkingModel} from '../../../parking/models/Parking.model'
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 export interface desc {
   fecha: Date
@@ -33,27 +34,19 @@ export interface desc {
 })
 export class CourtesyReportComponent implements OnInit {
   //@ViewChild(DataTableDirective)
-  @ViewChild(DxDataGridComponent, { static: false })
+  @ViewChild(DxDataGridComponent, {static: false})
   dataGrid!: DxDataGridComponent
   dtElement!: DataTableDirective
   dtOptions: DataTables.Settings = {}
   dtTrigger: Subject<any> = new Subject()
   pdfTable!: ElementRef
-
+  reportForm: FormGroup
   report: desc[] = []
   dataSource: any
-  parqueo: any
-
   nowDateTime = new Date()
-
   allParking: ParkingModel[] = Array<ParkingModel>()
   verTodosLosParqueosReport = environment.verTodosLosParqueosReport
-  @ViewChild('inputParking') inputParking!: ElementRef
-  fechaActual = new Date().toISOString().split('T')[0]
 
-  datosUsuarioLogeado = this.auth.getParking()
-  startDateReport: any
-  endDateReport: any
 
   constructor(
     private auth: AuthService,
@@ -61,42 +54,38 @@ export class CourtesyReportComponent implements OnInit {
     private messageService: MessageService,
     private utilitiesService: UtilitiesService,
     private authService: AuthService,
-    private permisionService: PermissionsService,
+    private permissionService: PermissionsService,
     private excelService: ReportService,
-    private parkingService: ParkingService
+    private parkingService: ParkingService,
+    private formBuilder: FormBuilder
   ) {
-    this.messageService.showLoading()
+    this.reportForm = this.createReportForm()
+  }
 
-    this.messageService.hideLoading()
+  get isSudo() {
+    return this.authService.isSudo
   }
 
   ngOnInit(): void {
     this.dtOptions = DataTableOptions.getSpanishOptions(10)
-    this.parkingService.getAllParking().then((data) => {
-      if (data.success) {
-        this.allParking = data.data.parkings
-      }
+
+    this.authService.user$.subscribe(({parkingId}) => {
+      this.reportForm.get('parkingId')?.setValue(parkingId)
+      this.getReport().then()
     })
 
-
+    this.parkingService.parkingLot$.subscribe((parkingLot) => {
+      this.allParking = parkingLot
+    })
   }
 
   ifHaveAction(action: string) {
-    return this.permisionService.ifHaveAction(action)
+    return this.permissionService.ifHaveAction(action)
   }
 
-  getInitialData() {
-
-  }
-
-  getCourtesyRpt() {
-
-    this.parqueo = this.datosUsuarioLogeado.id
-    if (this.ifHaveAction('verTodosLosParqueosReport')) {
-      this.parqueo = this.inputParking.nativeElement.value
-    }
+  getReport() {
     return this.reportService
-      .getCourtesyRpt(this.parqueo)
+      .getCourtesyRpt(this.reportForm.value.parkingId)
       .toPromise()
       .then((data) => {
         if (data.success) {
@@ -114,22 +103,7 @@ export class CourtesyReportComponent implements OnInit {
 
   ngAfterViewInit() {
     this.dtTrigger.next()
-    this.parqueo = this.datosUsuarioLogeado.id
-    this.getCourtesyRpt()
-  }
-
-  exportGrid() {
-    if (this.report.length == 0) {
-      this.messageService.infoTimeOut('No hay información para exportar')
-      return
-    }
-    const doc = new jsPDF()
-    exportDataGridToPdf({
-      jsPDFDocument: doc,
-      component: this.dataGrid.instance
-    }).then(() => {
-      doc.save('Duracion.pdf')
-    })
+    this.getReport().then()
   }
 
   onExporting(e: any) {
@@ -137,19 +111,7 @@ export class CourtesyReportComponent implements OnInit {
       this.messageService.infoTimeOut('No hay información para exportar')
       return
     }
-    /*     const context = this;
-        const workbook = new Workbook();
-        const worksheet = workbook.addWorksheet('General');
-
-        exportDataGrid({
-          component: context.dataGrid.instance,
-          worksheet: worksheet,
-          autoFilterEnabled: true,
-        }).then(() => {
-          workbook.xlsx.writeBuffer().then((buffer: any) => {
-            saveAs(new Blob([buffer], {type: 'application/octet-stream'}), 'Cortesias.xlsx');
-          })
-        }); */
+    const {parkingId} = this.reportForm.value
     const header = [
       '',
       'Fecha de Creación',
@@ -172,52 +134,52 @@ export class CourtesyReportComponent implements OnInit {
     worksheet.addRow([])
 
     const busienssRow = worksheet.addRow(['', '', '', 'ebiGO'])
-    busienssRow.font = { name: 'Calibri', family: 4, size: 11, bold: true }
-    busienssRow.alignment = { horizontal: 'center', vertical: 'middle' }
+    busienssRow.font = {name: 'Calibri', family: 4, size: 11, bold: true}
+    busienssRow.alignment = {horizontal: 'center', vertical: 'middle'}
     busienssRow.eachCell((cell, number) => {
       if (number > 1) {
         cell.border = {
-          top: { style: 'thin' },
-          left: { style: 'thin' },
-          bottom: { style: 'thin' },
-          right: { style: 'thin' }
+          top: {style: 'thin'},
+          left: {style: 'thin'},
+          bottom: {style: 'thin'},
+          right: {style: 'thin'}
         }
       }
     })
     worksheet.mergeCells('D2:M3')
     let ParqueoReporte = 'Todos los parqueos'
-    if (this.parqueo != '0') {
+    if (parkingId != '0') {
       const parqueoEncontrado = this.allParking.find(
-        (parqueos) => parqueos.id == this.parqueo
+        (parqueos) => parqueos.id == parkingId
       )
       if (parqueoEncontrado) {
         ParqueoReporte = parqueoEncontrado.name
       }
     }
     const addressRow = worksheet.addRow(['', '', '', ParqueoReporte])
-    addressRow.font = { name: 'Calibri', family: 4, size: 11, bold: true }
-    addressRow.alignment = { horizontal: 'center', vertical: 'middle' }
+    addressRow.font = {name: 'Calibri', family: 4, size: 11, bold: true}
+    addressRow.alignment = {horizontal: 'center', vertical: 'middle'}
     addressRow.eachCell((cell, number) => {
       if (number > 1) {
         cell.border = {
-          top: { style: 'thin' },
-          left: { style: 'thin' },
-          bottom: { style: 'thin' },
-          right: { style: 'thin' }
+          top: {style: 'thin'},
+          left: {style: 'thin'},
+          bottom: {style: 'thin'},
+          right: {style: 'thin'}
         }
       }
     })
     worksheet.mergeCells('D4:M5')
     const titleRow = worksheet.addRow(['', '', '', 'Reporte - Cortesias'])
-    titleRow.font = { name: 'Calibri', family: 4, size: 11, bold: true }
-    titleRow.alignment = { horizontal: 'center', vertical: 'middle' }
+    titleRow.font = {name: 'Calibri', family: 4, size: 11, bold: true}
+    titleRow.alignment = {horizontal: 'center', vertical: 'middle'}
     titleRow.eachCell((cell, number) => {
       if (number > 1) {
         cell.border = {
-          top: { style: 'thin' },
-          left: { style: 'thin' },
-          bottom: { style: 'thin' },
-          right: { style: 'thin' }
+          top: {style: 'thin'},
+          left: {style: 'thin'},
+          bottom: {style: 'thin'},
+          right: {style: 'thin'}
         }
       }
     })
@@ -231,38 +193,20 @@ export class CourtesyReportComponent implements OnInit {
     worksheet.addImage(logo, 'B3:C6')
     worksheet.addRow([])
     const infoRow = worksheet.addRow(['', 'Información General'])
-    infoRow.font = { name: 'Calibri', family: 4, size: 11, bold: true }
-    infoRow.alignment = { horizontal: 'center', vertical: 'middle' }
+    infoRow.font = {name: 'Calibri', family: 4, size: 11, bold: true}
+    infoRow.alignment = {horizontal: 'center', vertical: 'middle'}
     infoRow.eachCell((cell, number) => {
       if (number > 1) {
         cell.border = {
-          top: { style: 'thin' },
-          left: { style: 'thin' },
-          bottom: { style: 'thin' },
-          right: { style: 'thin' }
+          top: {style: 'thin'},
+          left: {style: 'thin'},
+          bottom: {style: 'thin'},
+          right: {style: 'thin'}
         }
       }
     })
     worksheet.mergeCells('B10:M11')
     worksheet.addRow([])
-    const header1 = worksheet.addRow([
-      '',
-      'Fecha Inicio: ' + this.startDateReport,
-      '',
-      '',
-      '',
-      'Fecha Fin: ' + this.endDateReport
-    ])
-    header1.eachCell((cell, number) => {
-      if (number > 1) {
-        cell.border = {
-          top: { style: 'thin' },
-          left: { style: 'thin' },
-          bottom: { style: 'thin' },
-          right: { style: 'thin' }
-        }
-      }
-    })
     worksheet.mergeCells('B13:E14')
     worksheet.mergeCells('F13:M14')
     const header2 = worksheet.addRow([
@@ -272,17 +216,17 @@ export class CourtesyReportComponent implements OnInit {
       '',
       '',
       'Documento generado: ' +
-        new Date().toLocaleDateString('es-GT') +
-        '  ' +
-        new Date().toLocaleTimeString()
+      new Date().toLocaleDateString('es-GT') +
+      '  ' +
+      new Date().toLocaleTimeString()
     ])
     header2.eachCell((cell, number) => {
       if (number > 1) {
         cell.border = {
-          top: { style: 'thin' },
-          left: { style: 'thin' },
-          bottom: { style: 'thin' },
-          right: { style: 'thin' }
+          top: {style: 'thin'},
+          left: {style: 'thin'},
+          bottom: {style: 'thin'},
+          right: {style: 'thin'}
         }
       }
     })
@@ -297,14 +241,14 @@ export class CourtesyReportComponent implements OnInit {
         cell.fill = {
           type: 'pattern',
           pattern: 'solid',
-          fgColor: { argb: 'FFFFFF00' },
-          bgColor: { argb: 'FF0000FF' }
+          fgColor: {argb: 'FFFFFF00'},
+          bgColor: {argb: 'FF0000FF'}
         }
         cell.border = {
-          top: { style: 'thin' },
-          left: { style: 'thin' },
-          bottom: { style: 'thin' },
-          right: { style: 'thin' }
+          top: {style: 'thin'},
+          left: {style: 'thin'},
+          bottom: {style: 'thin'},
+          right: {style: 'thin'}
         }
       }
     })
@@ -328,10 +272,10 @@ export class CourtesyReportComponent implements OnInit {
       row.eachCell((cell, number) => {
         if (number > 1) {
           cell.border = {
-            top: { style: 'thin' },
-            left: { style: 'thin' },
-            bottom: { style: 'thin' },
-            right: { style: 'thin' }
+            top: {style: 'thin'},
+            left: {style: 'thin'},
+            bottom: {style: 'thin'},
+            right: {style: 'thin'}
           }
         }
       })
@@ -339,33 +283,6 @@ export class CourtesyReportComponent implements OnInit {
     worksheet.addRow([])
     worksheet.addRow([])
     worksheet.addRow([])
-    /* let headerResumen = worksheet.addRow(['','Fecha','Total de vehiculos','Total de ingresos','Total de descuento','Total pagado']);
-    headerResumen.eachCell((cell, number) => {
-      if(number > 1){
-        cell.border = { top: { style: 'thin' }, left: { style: 'thin' }, bottom: { style: 'thin' }, right: { style: 'thin' } }
-      }
-    });
-    let groupData = this.dataSource.reduce((r:any, a:any) =>{
-      r[a.ep_entry_date.slice(0,10)] = [...r[a.ep_entry_date.slice(0,10)] || [], a];
-      return r;
-    },{});
-    Object.entries(groupData).forEach(([key,value]) => {
-      let valor = JSON.parse(JSON.stringify(value));
-      let total = 0;
-      let descuento = 0;
-      let pagado = 0;
-      valor.forEach((element:any) => {
-        total+= +element.total;
-        descuento+= +element.descuento;
-        pagado+= +element.pagado;
-      });
-      let detailResumen = worksheet.addRow(['',key,valor.length,total,descuento,pagado]);
-      detailResumen.eachCell((cell, number) => {
-        if(number > 1){
-          cell.border = { top: { style: 'thin' }, left: { style: 'thin' }, bottom: { style: 'thin' }, right: { style: 'thin' } }
-        }
-      });
-    }); */
 
     worksheet.getColumn(2).width = 25
     worksheet.getColumn(3).width = 25
@@ -384,14 +301,33 @@ export class CourtesyReportComponent implements OnInit {
     worksheet.getColumn(16).width = 15
     worksheet.getColumn(17).width = 15
 
-    //Generate Excel File with given name
     workbook.xlsx.writeBuffer().then((data) => {
       const blob = new Blob([data], {
         type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
       })
-      saveAs(blob, `Reporte de Cortesias - Generado - ${this.nowDateTime.toLocaleString()}.xlsx`)
+      saveAs(blob, `Reporte de Cortesías - Generado - ${this.nowDateTime.toLocaleString()}.xlsx`)
     })
     e.cancel = true
+  }
+
+  exportGrid() {
+    if (this.report.length == 0) {
+      this.messageService.infoTimeOut('No hay información para exportar')
+      return
+    }
+    const doc = new jsPDF()
+    exportDataGridToPdf({
+      jsPDFDocument: doc,
+      component: this.dataGrid.instance
+    }).then(() => {
+      doc.save('Duracion.pdf')
+    })
+  }
+
+  private createReportForm() {
+    return this.formBuilder.group({
+      parkingId: ['0']
+    })
   }
 
   private rerender() {
