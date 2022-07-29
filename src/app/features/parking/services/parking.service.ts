4c889e443de73a5637d23a424d4e2e4d20890fea
aleja <alejandrocordova198@gmail.com>
2022-07-29 17:06:17 -0600

    (E-205) Adding Observer of Parking and parking lot to Create  component and Adding Reactive Forms on Billing Data Component.

--- src/app/features/parking/services/parking.service.ts
+++ src/app/features/parking/services/parking.service.ts
@@ -1,8 +1,8 @@
-import {Injectable, OnDestroy} from '@angular/core'
-import {environment} from '../../../../environments/environment'
-import {HttpClient} from '@angular/common/http'
-import {MessageService} from '../../../shared/services/message.service'
-import {ResponseModel} from '../../../shared/model/Request.model'
+import { Injectable, OnDestroy } from '@angular/core'
+import { environment } from '../../../../environments/environment'
+import { HttpClient } from '@angular/common/http'
+import { MessageService } from '../../../shared/services/message.service'
+import { ResponseModel } from '../../../shared/model/Request.model'
 import {
   AccessModel,
   CreateParkingFileModel,
@@ -11,15 +11,24 @@ import {
   CreateParkingStepOneModel,
   CreateParkingStepTwoModel
 } from '../models/CreateParking.model'
-import {Router} from '@angular/router'
-import {map} from 'rxjs/operators'
-import {CurrencyOptionModel, Day, PaymentMethodModel, SettingsOptionsModel} from '../models/SettingsOption.model'
-import {BehaviorSubject, Observable, Subscribable} from 'rxjs'
-import {CountriesModel} from '../models/Countries.model'
-import {CreateTariffModel} from '../models/Tariff.model'
-import {CreateProfilesModel} from '../models/MontlyParking.model'
-import {CreateStation, CreateStationaryCourtesy, StationsCourtesyModel} from '../models/StationaryCourtesy.model'
-import {ParkedModel, ParkingModel} from '../models/Parking.model'
+import { Router } from '@angular/router'
+import { map } from 'rxjs/operators'
+import {
+  CurrencyOptionModel,
+  Day,
+  PaymentMethodModel,
+  SettingsOptionsModel
+} from '../models/SettingsOption.model'
+import { BehaviorSubject, Observable, Subscribable } from 'rxjs'
+import { CountriesModel } from '../models/Countries.model'
+import { CreateTariffModel } from '../models/Tariff.model'
+import { CreateProfilesModel } from '../models/MontlyParking.model'
+import {
+  CreateStation,
+  CreateStationaryCourtesy,
+  StationsCourtesyModel
+} from '../models/StationaryCourtesy.model'
+import { ParkedModel, ParkingModel } from '../models/Parking.model'
 
 @Injectable({
   providedIn: 'root'
@@ -35,31 +44,27 @@ export class ParkingService implements OnDestroy {
   countries: CountriesModel[] = new Array<CountriesModel>()
   allParkingLot: ParkingModel[] = []
   private apiUrl = environment.serverAPI
-  private parkingLotSubject$: BehaviorSubject<ParkingModel[]> = new BehaviorSubject<ParkingModel[]>([])
-  public parkingLot$: Observable<ParkingModel[]> = this.parkingLotSubject$.asObservable()
+  private parkingLotSubject$: BehaviorSubject<ParkingModel[]> =
+    new BehaviorSubject<ParkingModel[]>([])
+  public parkingLot$: Observable<ParkingModel[]> =
+    this.parkingLotSubject$.asObservable()
 
   constructor(
     private http: HttpClient,
     private message: MessageService,
-    private route: Router,
+    private route: Router
   ) {
-    this.getCountries()
-      .toPromise()
-      .then((data) => {
-        this.countries = data.data
+    Promise.all([
+      this.getCountries()
+        .toPromise()
+        .then((data) => {
+          this.countries = data.data
+        }),
+      this.getSettingsOptions().toPromise().then(),
+      this.getAllParking().then((data) => {
+        this.parkingLotSubject$.next(data.data.parkings)
       })
-      .then(() => {
-        this.getSettingsOptions()
-          .toPromise()
-          .then((data) => {
-            this.settingsOptions = data
-          })
-      }).then(() => {
-
-    })
-    this.getAllParking().then(data => {
-      this.parkingLotSubject$.next(data.data.parkings)
-    })
+    ]).then()
   }
 
   getCountries() {
@@ -72,7 +77,8 @@ export class ParkingService implements OnDestroy {
       .pipe(
         map((data) => {
           if (!data.success) throw data.message
-          return {...data.data}
+          this.settingsOptions = data.data
+          return { ...data.data }
         })
       )
   }
@@ -86,10 +92,10 @@ export class ParkingService implements OnDestroy {
 
   getAccesses(): Array<AccessModel> {
     return [
-      {id: 0, name: 'Entrada primaria'},
-      {id: 1, name: 'Salida primaria'},
-      {id: 2, name: 'Entrada secundaria'},
-      {id: 3, name: 'Salida secundaria'}
+      { id: 0, name: 'Entrada primaria' },
+      { id: 1, name: 'Salida primaria' },
+      { id: 2, name: 'Entrada secundaria' },
+      { id: 3, name: 'Salida secundaria' }
     ]
   }
 
@@ -106,7 +112,7 @@ export class ParkingService implements OnDestroy {
       )
   }
 
-//TODO: Eliminar este método
+  //TODO: Eliminar este método
   getAllParkingLot(): ParkingModel[] {
     return this.allParkingLot
   }
@@ -203,7 +209,7 @@ export class ParkingService implements OnDestroy {
   getQR(stationID: string): Observable<Blob> {
     return this.http.get(
       `${this.apiUrl}backoffice/parking/station/${stationID}/downloadqr`,
-      {responseType: 'blob'}
+      { responseType: 'blob' }
     )
   }
 
@@ -308,7 +314,7 @@ export class ParkingService implements OnDestroy {
       .toPromise()
   }
 
-//TODO: Hacer privado este metodo
+  //TODO: Hacer privado este metodo
   getAllParking() {
     return this.http
       .get<ResponseModel>(`${this.apiUrl}backoffice/parking/enables`)
@@ -349,7 +355,11 @@ export class ParkingService implements OnDestroy {
       )
   }
 
-  getOutParked(parkedId: string, payment_method: number, dateOutToGetOut: Date) {
+  getOutParked(
+    parkedId: string,
+    payment_method: number,
+    dateOutToGetOut: Date
+  ) {
     return this.http
       .post<ResponseModel>(`${this.apiUrl}backoffice/parking/getOut`, {
         parkedId,
@@ -391,21 +401,17 @@ export class ParkingService implements OnDestroy {
   }
 
   createStationWithCourtesy(newStation: CreateStation) {
-    return this.http
-      .post<ResponseModel>(
-        `${this.apiUrl}backoffice/station_cortesy/station`,
-        newStation
-      )
-
+    return this.http.post<ResponseModel>(
+      `${this.apiUrl}backoffice/station_cortesy/station`,
+      newStation
+    )
   }
 
   editStationWithCourtesy(editStation: CreateStation) {
-    return this.http
-      .put<ResponseModel>(
-        `${this.apiUrl}backoffice/station_cortesy/station/${editStation.id}`,
-        editStation
-      )
-
+    return this.http.put<ResponseModel>(
+      `${this.apiUrl}backoffice/station_cortesy/station/${editStation.id}`,
+      editStation
+    )
   }
 
   deleteAntennaWithCourtesy(id: string) {
@@ -425,12 +431,10 @@ export class ParkingService implements OnDestroy {
   }
 
   editStationaryCourtesy(editStationCourtesy: CreateStationaryCourtesy) {
-    return this.http
-      .put<ResponseModel>(
-        `${this.apiUrl}backoffice/station_cortesy/courtesy/${editStationCourtesy.id}`,
-        editStationCourtesy
-      )
-
+    return this.http.put<ResponseModel>(
+      `${this.apiUrl}backoffice/station_cortesy/courtesy/${editStationCourtesy.id}`,
+      editStationCourtesy
+    )
   }
 
   getParkingInfo(parkingId: string) {
@@ -461,11 +465,10 @@ export class ParkingService implements OnDestroy {
     return this.http
       .put<ResponseModel>(
         `${this.apiUrl}backoffice/tariff/rule/active/${parkingId}`,
-        {isActive: newStatus}
+        { isActive: newStatus }
       )
       .toPromise()
   }
-
 
   ngOnDestroy(): void {
     this.parkingLotSubject$.unsubscribe()
--- src/app/features/shared/billing-data/billing-data.component.ts
+++ src/app/features/shared/billing-data/billing-data.component.ts
@@ -1,13 +1,13 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core'
-import {UntypedFormBuilder, UntypedFormGroup, Validators} from '@angular/forms'
-import {MessageService} from '../../../shared/services/message.service'
-import {ParkingService} from '../../parking/services/parking.service'
-import {UtilitiesService} from '../../../shared/services/utilities.service'
-import {SettingsOptionsModel} from '../../parking/models/SettingsOption.model'
-import {CreateParkingStepFourModel} from '../../parking/models/CreateParking.model'
-import {Router} from '@angular/router'
-import {ParkingModel} from '../../parking/models/Parking.model'
-import {AuthService} from '../../../shared/services/auth.service'
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { MessageService } from '../../../shared/services/message.service'
+import { ParkingService } from '../../parking/services/parking.service'
+import { UtilitiesService } from '../../../shared/services/utilities.service'
+import { SettingsOptionsModel } from '../../parking/models/SettingsOption.model'
+import { CreateParkingStepFourModel } from '../../parking/models/CreateParking.model'
+import { Router } from '@angular/router'
+import { ParkingModel } from '../../parking/models/Parking.model'
+import { AuthService } from '../../../shared/services/auth.service'
 
 @Component({
   selector: 'app-billing-data',
@@ -15,8 +15,8 @@ import {AuthService} from '../../../shared/services/auth.service'
   styleUrls: ['./billing-data.component.css']
 })
 export class BillingDataComponent implements OnInit {
-  stepFourForm: UntypedFormGroup = this.createForm()
-  settingsOptions!: SettingsOptionsModel
+  stepFourForm: FormGroup = this.createForm()
+  settingsOptions: SettingsOptionsModel
   allParking: ParkingModel[] = []
 
   @Input() parkingId!: string
@@ -28,23 +28,16 @@ export class BillingDataComponent implements OnInit {
   constructor(
     private message: MessageService,
     private parkingService: ParkingService,
-    private formBuilder: UntypedFormBuilder,
+    private formBuilder: FormBuilder,
     private utilitiesService: UtilitiesService,
     private route: Router,
     private authService: AuthService
   ) {
+    this.settingsOptions = this.parkingService.settingsOptions
   }
 
   get isSudo() {
     return this.authService.isSudo
-  }
-
-  async setPaymentData(parkingId: string) {
-    const paymentInfo = await this.parkingService.getParkingInfo(parkingId).toPromise();
-    this.stepFourForm.get('business_address')?.setValue(paymentInfo.business_address)
-    this.stepFourForm.get('business_name')?.setValue(paymentInfo.business_name)
-    this.stepFourForm.get('currency')?.setValue(paymentInfo.currency)
-    this.stepFourForm.get('nit')?.setValue(paymentInfo.nit)
   }
 
   get isVisaSelected() {
@@ -55,6 +48,18 @@ export class BillingDataComponent implements OnInit {
   get isBacSelected() {
     const selected = this.stepFourForm.get('pay_method')?.value
     return selected != 0 && selected
+  }
+
+  async setPaymentData(parkingId: string) {
+    const paymentInfo = await this.parkingService
+      .getParkingInfo(parkingId)
+      .toPromise()
+    this.stepFourForm
+      .get('business_address')
+      ?.setValue(paymentInfo.business_address)
+    this.stepFourForm.get('business_name')?.setValue(paymentInfo.business_name)
+    this.stepFourForm.get('currency')?.setValue(paymentInfo.currency)
+    this.stepFourForm.get('nit')?.setValue(paymentInfo.nit)
   }
 
   addValidators() {
@@ -93,27 +98,24 @@ export class BillingDataComponent implements OnInit {
       if (this.showNavigationButtons && !this.isPublic) {
         this.changeStep.emit(number)
       } else {
-        this.endToken()
+        this.endToken().then()
         this.route.navigate(['/']).catch()
       }
-
     } else {
       this.message.hideLoading()
       this.changeStep.emit(number)
     }
   }
 
-  parkingIdSelected() {
+  async parkingIdSelected() {
     this.message.showLoading()
-    this.parkingId = this.stepFourForm.get('parkingId')?.value ? this.stepFourForm.get('parkingId')?.value : this.parkingId
+    this.parkingId = this.stepFourForm.value.parkingId
+      ? this.stepFourForm.get('parkingId')?.value
+      : this.parkingId
     if (this.parkingId) {
-      this.setPaymentData(this.parkingId).catch()
+      await this.setPaymentData(this.parkingId)
     }
     this.message.hideLoading()
-  }
-
-  private async endToken() {
-    return this.parkingService.endTempToken().toPromise().then()
   }
 
   createForm() {
@@ -162,23 +164,21 @@ export class BillingDataComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.settingsOptions = this.parkingService.settingsOptions
+    console.log(this.settingsOptions)
     if (!this.isCreatingParking) {
-      this.stepFourForm.get('parkingId')?.setValue(this.parkingId)
-      this.getParkingLot().then()
-      this.parkingIdSelected()
+      this.parkingService.parkingLot$.subscribe((parkingLot) => {
+        this.allParking = parkingLot
+      })
+      this.authService.user$.subscribe(({ parkingId }) => {
+        this.parkingId = parkingId
+        this.stepFourForm.get('parkingId')?.setValue(parkingId)
+        this.parkingIdSelected().then()
+      })
     }
   }
 
-  getParkingLot() {
-    return this.parkingService.getAllParking().then(x => {
-      if (x.success) {
-        this.allParking = x.data.parkings
-      } else {
-        this.message.error(x.message)
-        return
-      }
-    })
+  private async endToken() {
+    return this.parkingService.endTempToken().toPromise().then()
   }
 
   private getStepFour(): CreateParkingStepFourModel {
@@ -188,9 +188,9 @@ export class BillingDataComponent implements OnInit {
       business_name: this.stepFourForm.controls['business_name'].value,
       currency: this.stepFourForm.controls['currency'].value,
       is_our_bac_credential:
-      this.stepFourForm.controls['is_our_bac_credential'].value,
+        this.stepFourForm.controls['is_our_bac_credential'].value,
       is_our_visa_credential:
-      this.stepFourForm.controls['is_our_visa_credential'].value,
+        this.stepFourForm.controls['is_our_visa_credential'].value,
       nit: this.stepFourForm.controls['nit'].value,
       pay_method: this.stepFourForm.controls['pay_method'].value,
       visa_credential: {
@@ -198,7 +198,7 @@ export class BillingDataComponent implements OnInit {
         terminal: this.stepFourForm.controls['terminal'].value,
         merchant_user: this.stepFourForm.controls['merchant_id_visa'].value,
         merchant_password:
-        this.stepFourForm.controls['merchant_pass_visa'].value
+          this.stepFourForm.controls['merchant_pass_visa'].value
       },
       bac_credential: {
         url: this.stepFourForm.controls['url_bac'].value,
@@ -206,7 +206,7 @@ export class BillingDataComponent implements OnInit {
         acquirer_id: this.stepFourForm.controls['acquirer_id_bac'].value,
         pmtnpssw: this.stepFourForm.controls['pmtnpssw_bac'].value,
         purchase_currency:
-        this.stepFourForm.controls['purchase_currency_bac'].value
+          this.stepFourForm.controls['purchase_currency_bac'].value
       }
     }
   }
